a29284b8479e191857d9ae00ffc63ff3be29f05e
ekta <ektamo0123@gmail.com>
2022-08-25 05:18:47 +0530

    Added : check two lines using Oops concepts

--- LineCompatison.java
+++ LineCompatison.java
@@ -3,13 +3,14 @@ package linecomparison;
 import java.util.Scanner;
 
 public class LineCompatison {
-	static void checkEquality(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
-        Double length1 = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-        System.out.println("Length Of First Line : "+length1);
-
-        Double length2 = Math.sqrt((x4-x3)*(x4-x3) + (y4-y3)*(y4-y3));
-        System.out.println("Length Of Second Line : "+length2);
-
+	public void equalsMethod(Double length1, Double length2) {
+        if (length1.equals(length2)) {
+            System.out.println("Both the lines are equal");
+        } else {
+            System.out.println("lines are not equal");
+        }
+    }
+    public void compareToMethod(Double length1, Double length2) {
         int compare = length1.compareTo(length2);
         if (compare < 0) {
             System.out.println("Length of first line is less than length of second line");
@@ -40,6 +41,16 @@ public class LineCompatison {
         int x4 = sc.nextInt();
         System.out.print("y4 : ");
         int y4 = sc.nextInt();
-        checkEquality(x1, y1, x2, y2, x3, y3, x4, y4);
+        
+        double length1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println("Length Of First Line : " + length1);
+
+        double length2 = Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
+        System.out.println("Length Of Second Line : " + length2);
+
+        LineCompatison obj = new LineCompatison();
+
+        obj.equalsMethod(length1, length2);
+        obj.compareToMethod(length1, length2);
     }
 }
